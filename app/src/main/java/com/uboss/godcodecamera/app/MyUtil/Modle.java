f6415d1e638ad9e5455205b66f7c4d06115390b9
崔焱 <cuiyan@cuiyandeMacBook-Pro.local>
2016-02-18 12:05:11 +0800

    model view done

---
+++ app/src/main/java/com/uboss/godcodecamera/app/MyUtil/Modle.java
@@ -0,0 +1,13 @@
+package com.uboss.godcodecamera.app.MyUtil;
+
+/**
+ * Created by cuiyan on 16/2/18.
+ */
+public class Modle {
+    private int max_pic;
+
+
+    public Modle(int max_pic){
+        this.max_pic=max_pic;
+    }
+}
--- app/src/main/java/com/uboss/godcodecamera/app/ui/MakeQrcodeActivity.java
+++ app/src/main/java/com/uboss/godcodecamera/app/ui/MakeQrcodeActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
@@ -31,7 +32,7 @@ public class MakeQrcodeActivity extends AppCompatActivity {
     static ArrayList<String> arr_model_instruction = new ArrayList<String>();
     static ArrayList<Integer> addonList = new ArrayList<Integer>();
     ArrayList<HashMap<String,Object>> models = new ArrayList<>();
-
+    SimpleAdapter model_adapter;
     static{
         Log.i(TAG,"addonList static");
         addonList.add(R.drawable.sticker1);
@@ -59,7 +60,10 @@ public class MakeQrcodeActivity extends AppCompatActivity {
     HListView bottomToolBar;    //--cyan 包括贴纸，滤镜，标签选择 HListView 是一个横向的listview类，包括在gradle中
     @InjectView(R.id.model_area)
     ViewGroup toolArea;
-
+    @InjectView(R.id.et_qrcode_content_up)
+    EditText et_qrcode_content_up;
+    @InjectView(R.id.et_qrcode_content_down)
+    EditText et_qrcode_content_down;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,9 +78,10 @@ public class MakeQrcodeActivity extends AppCompatActivity {
         if(bottomToolBar == null){
             Log.i(TAG,"bottomToolBar == null");
         }
-        bottomToolBar.setAdapter(new SimpleAdapter(MakeQrcodeActivity.this,getToolList(),R.layout.item_bottom_toolbar,
+        model_adapter= new SimpleAdapter(MakeQrcodeActivity.this,getToolList(),R.layout.item_bottom_toolbar,
                 new String[]{"title","img","instruction"},
-                new int[]{R.id.tv_model_title,R.id.img_model,R.id.tv_model_instruction}));
+                new int[]{R.id.tv_model_title,R.id.img_model,R.id.tv_model_instruction});
+        bottomToolBar.setAdapter(model_adapter);
 
         bottomToolBar.setOnItemClickListener( new it.sephiroth.android.library.widget.AdapterView.OnItemClickListener() {
 
@@ -84,7 +89,39 @@ public class MakeQrcodeActivity extends AppCompatActivity {
             public void onItemClick(it.sephiroth.android.library.widget.AdapterView<?> arg0,
                                     View arg1, int position, long arg3) {
                 Toast.makeText(MakeQrcodeActivity.this,position+"",Toast.LENGTH_SHORT).show();
+                switch (position){
+                    case 0:
+                        //先文后图
+                        changeModel(0,R.drawable.ic_launcher);
+                        changeModel(1,addonList.get(1));
+                        et_qrcode_content_down.setVisibility(View.GONE);
+                        et_qrcode_content_up.setVisibility(View.VISIBLE);
 
+                        model_adapter.notifyDataSetChanged();
+                        break;
+                    case 1:
+                        //先图后文
+                        changeModel(1,R.drawable.ic_launcher);
+                        changeModel(0,addonList.get(0));
+                        et_qrcode_content_up.setVisibility(View.GONE);
+                        et_qrcode_content_down.setVisibility(View.VISIBLE);
+
+//                        HashMap<String, Object> map = new HashMap<String, Object>();
+//                        map.put("title",arr_model_title.get(1));
+//                        map.put("img",R.drawable.ic_launcher);
+//                        map.put("instruction",arr_model_instruction.get(1));
+//                        models.set(1,map);
+//
+//                        map.put("title",arr_model_title.get(0));
+//                        map.put("img",addonList.get(0));
+//                        map.put("instruction",arr_model_instruction.get(0));
+//                        models.set(0,map);
+                        model_adapter.notifyDataSetChanged();
+                        break;
+                    default:
+                        break;
+
+                }
 //                labelSelector.hide();       //之前选择的贴纸隐藏
 //                Addon sticker = EffectUtil.addonList.get(arg2);
 //                EffectUtil.addStickerImage(mImageView, PhotoProcessActivity.this, sticker,
@@ -97,6 +134,15 @@ public class MakeQrcodeActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void changeModel(int position,int img_id){
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
+        hashMap.put("title",arr_model_title.get(position));
+        hashMap.put("img",img_id);
+        hashMap.put("instruction",arr_model_instruction.get(position));
+        models.set(position,hashMap);
+    }
+
     private ArrayList<HashMap<String, Object>> getToolList() {
         for(int i=0;i<addonList.size();i++){
             HashMap<String, Object> hashMap = new HashMap<String, Object>();
@@ -108,6 +154,7 @@ public class MakeQrcodeActivity extends AppCompatActivity {
 
         return models;
     }
+
     private void setModelData(){
 //        Resources res = MakeQrcodeActivity.this.getResources();
 //
